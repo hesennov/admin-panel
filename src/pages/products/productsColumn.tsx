a9db3cc1a717fd6dbe8e10582ad997c41622670e
Hesennov <hasanhasanov345@gmail.com>
2026-02-28 14:58:25 -0600

    productsColumn

--- src/pages/products/productsColumn.tsx
+++ src/pages/products/productsColumn.tsx
@@ -1,7 +1,10 @@
 import type { Column } from "../../types/user";
 import type { Products } from "../../types/Products";
 
-export const productsColumn = (): Column<Products>[] => [
+export const productsColumn = (actions: {
+  onDelete: (id: number) => void;
+  onEdit: (id: number) => void;
+}): Column<Products>[] => [
   { label: "Title", key: "title" },
   { label: "Price", key: "price" },
   {
@@ -18,6 +21,32 @@ export const productsColumn = (): Column<Products>[] => [
           }`}
         >
           {value} {stockValue <= 5 ? "⚠️ Low Stock" : "In Stock"}
+        </div>
+      );
+    },
+  },
+  {
+    label: "Actions",
+    key: "id",
+    render: (_, row) => {
+      return (
+        <div className="flex gap-3">
+          {actions.onDelete && (
+            <span
+              className="px-2 py-1 bg-red-500 text-white"
+              onClick={() => actions.onDelete(row.id)}
+            >
+              DElete
+            </span>
+          )}
+          {actions.onEdit && (
+            <span
+              className="bg-yellow-500 px-2 py-1 text-white"
+              onClick={() => actions.onEdit(row.id)}
+            >
+              EDIT
+            </span>
+          )}
         </div>
       );
     },
