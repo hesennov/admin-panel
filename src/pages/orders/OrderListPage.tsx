fb0a7692d2b8339aeec2f7d215eb5593aed0db1a
Hesennov <hasanhasanov345@gmail.com>
2026-03-16 00:36:56 -0600

    end3/16

--- src/pages/orders/OrderListPage.tsx
+++ src/pages/orders/OrderListPage.tsx
@@ -1,10 +1,11 @@
+import StatusView from "../../components/common/StatusView";
 import ReusablePagination from "../../components/reusablePagination/ReusablePagination";
 import ReusableTable from "../../components/reusableTable/ReusableTable";
 import { useOrders } from "../../hooks/useOrders";
 import { ordersColumn } from "./ordersColumn";
 
 export default function OrderListPage() {
-  const { loading, page, orders, setPage, totalPages,  } =
+  const { loading, page, orders, setPage, totalPages,error,fetchOrders  } =
     useOrders();
 
   const handleDelete = () => {
@@ -13,7 +14,23 @@ export default function OrderListPage() {
   const handleEdit = () => {
     console.log("edited user");
   };
+ if (loading) {
+    return (
+      <div className="flex items-center justify-center h-64">
+        <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-indigo-600"></div>
+      </div>
+    );
+  }
 
+    if (error) {
+      return (
+        <StatusView
+          type="error"
+          message="Could not connect to the server.Please make sure the API is running, amigo!!"
+          onRetry={fetchOrders}
+        />
+      );
+    }
   return (
     <div>
       <ReusableTable
